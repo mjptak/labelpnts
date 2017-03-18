0bedae033c5bf23627c8cd4a963220c298980c96
mark ptak <mjptak@gmail.com>
2017-03-18 09:14:24 -0500

    initial coding to add endpoints

--- ppoint.py
+++ ppoint.py
@@ -51,36 +51,54 @@ class Pgon:
         return len(self.edgelist)
 def readLines():
     llist=[]
+    lcnt = 0
+    plist = []
     with open("2013_leaders.csv") as g:
+        linecnt = 0
+        pntcnt = 0
         for line in g:
-		    llist.append(line)
-	return llist
-            		
+            a = line.split(',')
+            try:
+                spnt = (float(a[2]),float(a[3]))
+                epnt = (float(a[4]),float(a[5]))
+            except:
+                spnt = (0,0)
+                #print line
+                epnt = (0,0)
+            if spnt not in plist:
+                plist.append(spnt)
+            if epnt not in plist:
+                plist.append(epnt)
+            lcnt+=1
+    print "total number of leader segments = %s" %lcnt
+    return plist
+                    
         
 
 #begin main program
 import math
 import os
-		
+        
 pntsdict = {}
 with open("2013_labels.csv") as f:
     for line in f:
-        print line
+        #print line
         tmp = line.split(',')
         pnt = Ppoint(tmp[4],tmp[2],tmp[3])
-		#check for duplicate labels before storing
+        #check for duplicate labels before storing by cycling through keys
         pntsdict[pnt.locreport()[0]] = pnt
 
 tolerance = 0.1
 print len(pntsdict)
+tpntlist = readLines()  #temp check to get unique leader point count
+print len (tpntlist)
 
 
 for k,v in pntsdict.iteritems():
-    print k,v.locreport()[0]
+    #print k,v.locreport()[0]
+    continue
 
-llist = readLines()
-for each in llist:
-    print each
+
 
 #probably a much cleaner way to encapsulate points
 
